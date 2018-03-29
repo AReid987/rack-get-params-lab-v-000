ceca3017ce1796652535c02c83d629694d640adf
Antonio Reid <read.musik@gmail.com>
2018-03-29 19:27:14 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -13,6 +13,10 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.size == 0
+        resp.write "Your cart is empty"
+      end
     else
       resp.write "Path Not Found"
     end
